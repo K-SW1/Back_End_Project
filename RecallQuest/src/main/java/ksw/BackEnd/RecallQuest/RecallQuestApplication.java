fa330d019819a9b7a00f8af49db29b013bd0535a
김예나 <yn112511@naver.com>
2024-03-06 06:09:58 +0900

    feat:Config
    환경설정 커밋

---
+++ RecallQuest/src/main/java/ksw/BackEnd/RecallQuest/RecallQuestApplication.java
@@ -0,0 +1,13 @@
+package ksw.BackEnd.RecallQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecallQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecallQuestApplication.class, args);
+	}
+
+}
