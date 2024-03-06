bd467d2bc687d3bc3012bfd2c61cc2a943f1bf0c
최성준 <choi000803@naver.com>
2024-03-06 20:20:31 +0900

    feat: test

---
+++ src/test/java/com/example/recallquestbackend/RecallQuestBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recallquestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecallQuestBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
